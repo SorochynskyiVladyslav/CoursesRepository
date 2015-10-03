a8c82a86eb8e669b09dd9e6ba821f160327acb4b
SorochynskyiVladyslav <vladyslav.sorochynskyi@gmail.com>
2015-10-04 00:35:57 +0300

    completed condition.c in lab1

---
+++ courses/prog_base/labs/lab1/condition.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int satisfies(int, int, int);
+
+int mymin(int a, int b){
+if (a>b) return b;
+else return a;
+}
+int mymax(int a, int b){
+if (a>b) return a;
+else return b;
+}
+
+int main() {
+    int aval = 0, bval = 0, cval = 0;
+    int resultVal;
+
+    resultVal = satisfies(aval, bval, cval);
+
+    printf("%i", resultVal);
+    return 0;
+}
+// copy the code below to the answers files and add #include <math.h> at the beginning
+int satisfies(int a, int b, int c) {
+   int result, modmin1, modmin2, modmin, modmax1, modmax2, modmax, sum2;
+
+   if ((a<0)&&(b<0)&&(c<0))
+   {modmin1=mymin(a, b);
+   modmin2=mymin(b, c);
+   modmin=mymin(modmin1, modmin2);
+     if (modmin==a) sum2=b+c;
+     else if (modmin==b) sum2=a+c;
+          else sum2=a+b;
+     if ((sum2<-256)&&(modmin<256)&&(modmin%2==0))
+        result=1;
+     else if (((abs(sum2)-modmin)<16)||(abs(sum2)<16))
+        result=1;
+        else result=0;}
+   else if ((a>=0)&&(b>=0)&&(c>=0))
+        {modmax1=mymax(a, b);
+        modmax2=mymax(b, c);
+        modmax=mymax(modmax1, modmax2);
+        modmin1=mymin(a, b);
+        modmin2=mymin(b, c);
+        modmin=mymin(modmin1, modmin2);
+          if ((modmax+modmin)<256)
+            result=1;
+            else result=0;}
+        else if ((a<0)||(b<0)||(c<0))
+             {modmin1=mymin(a, b);
+             modmin2=mymin(b, c);
+             modmin=mymin(modmin1, modmin2);
+               if (modmin>-256) result=1;
+                else result=0;}
+             else if ((a<b)&&(b<c))
+                    {sum2=(a+b)*7;
+                    if (sum2>-256) result=1;
+                    else result=0;}
+                  else if ((a<b)&&(c<b))
+                         {sum2=(a+c)*7;
+                         if (sum2>-256) result=1;
+                           else result=0;}
+                         else sum2=(b+c)*7;
+                            if (sum2>-256) result=1;
+                            else result=0;
+   return result;
+}
