265a2c32c67e3e8acdc5300b09ba76007f16a727
SorochynskyiVladyslav <vladyslav.sorochynskyi@gmail.com>
2015-10-04 21:52:51 +0300

    correction of math.c in lab1

--- courses/prog_base/labs/lab1/math.c
+++ courses/prog_base/labs/lab1/math.c
@@ -4,7 +4,7 @@
 
 double calc(double x, double y, double z) {
    double a;
-   if ((x==y)||(x==0)||(sin(x)==0))
+   if ((x==y)||(x==0)||(sin(x)==0)||(z==0))
     return NAN;
     else
    a=(pow(x, y+1))/(pow(x-y,1/z))+6*y+z/x+pow(abs(cos(y)/sin(x)+1), 1/2);
