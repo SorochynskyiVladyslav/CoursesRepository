c826d6ef608df52c56b058d33a57de510d6c6de6
SorochynskyiVladyslav <vladyslav.sorochynskyi@gmail.com>
2015-10-05 18:01:04 +0300

    corrected math.c in lab1

--- courses/prog_base/labs/lab1/math.c
+++ courses/prog_base/labs/lab1/math.c
@@ -4,9 +4,12 @@
 
 double calc(double x, double y, double z) {
    double a;
-   if ((x==y)||(x==0)||(sin(x)==0)||(z==0))
+   if ((x<=y)||(x<=0)||(sin(x)==0)||(z==0))
     return NAN;
     else
-   a=(pow(x, y+1))/(pow(x-y,1/z))+6*y+z/x+pow(abs(cos(y)/sin(x)+1), 1/2);
+    a0=pow(x, y+1)/pow(x-y, 1/z);
+    a1=6*y+z/x;
+    a2=pow(fabs(cos(y)/sin(x)+1), 1/2);
+    a=a0+a1+a2;
    return a;
 }
