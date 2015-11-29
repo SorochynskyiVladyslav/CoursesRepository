460ddb42887e6be56321489f3dd59d6e28ebfa3f
SorochynskyiVladyslav <vladyslav.sorochynskyi@gmail.com>
2015-11-29 20:05:40 +0200

    corrected files

--- courses/prog_base/tasks/files/files.c
+++ courses/prog_base/tasks/files/files.c
@@ -3,7 +3,7 @@
 void fprocess(const char * pread, const char * pwrite){
     int i, x1, x2, y1, y2;
     FILE * fp = NULL;
-    fp = fopen("pread.txt", "r");
+    fp = fopen(pread, "r");
     if (fp == NULL){
         printf("error opening file");
     }
@@ -15,7 +15,7 @@ void fprocess(const char * pread, const char * pwrite){
     }
     fscanf(fp, "%i%i%i%i", &x1, &y1, &x2, &y2);
     fclose(fp);
-    fp = fopen("pwrite.txt", "w");
+    fp = fopen(pwrite, "w");
     if (fp == NULL){
         printf("error opening file");
     }
