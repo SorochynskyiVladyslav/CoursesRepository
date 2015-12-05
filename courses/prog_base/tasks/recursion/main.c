32996d266872c17b4c3a6060f9095d80e1e52ca0
SorochynskyiVladyslav <vladyslav.sorochynskyi@gmail.com>
2015-12-05 12:07:26 +0200

    corrected recursion

--- courses/prog_base/tasks/recursion/main.c
+++ courses/prog_base/tasks/recursion/main.c
@@ -1,39 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int sumLoop(int num){
-    int s = num;
-    while (--num> 0 ){
-        s += num;
-    }
-    return s;
-}
-
-int sum(int num){
-    if (num == 0){
-        return 0;
-    } else {
-        return num + sum(num - 1);
-    }
-}
+int checkStr(char* str, char* p, int num);
 
 int main()
 {
-    int n = 5, s;
-    s = sum(n);
-    printf("%i", s);
+    char ch[100];
+    char* str;
+    int num = 11;
+    puts("enter a string");
+    str = gets(ch);
+    char* p = str + strlen(str) - 1;
+    num = checkStr(str, p, num);
+    printf("%i", num);
     return 0;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+int checkStr(char* str, char* p, int num){
+    if (*p >= 48 && *p <= 57 ) num = (int) *p - 48;
+    else num = 11;
+    if (p + 1 == str || num != 11){
+        if (p + 1 == str && num == 11){
+        num = 0;
+        }
+        return num;
+    }
+    else {
+        return checkStr(str, p - 1, num);
+    }
+}
