a1fde275715445b4a27e467c61cb49cff3177653
SorochynskyiVladyslav <vladyslav.sorochynskyi@gmail.com>
2016-04-17 21:52:29 +0300

    Started work on async server

---
+++ courses/prog_base_3/project/Server/func.cpp
@@ -0,0 +1,6 @@
+#include "servheader.h"
+
+Client :: Client (SOCKET socket, string nam) {
+    s = socket;
+    name = nam;
+}
--- courses/prog_base_3/project/Server/main.cpp
+++ courses/prog_base_3/project/Server/main.cpp
@@ -1,86 +1,59 @@
-#include <iostream>
-#include <io.h>
-#include <stdio.h>
-#include <winsock2.h>
+#include "servheader.h"
 
-#pragma comment(lib,"ws2_32.lib") //Winsock Library
-
-using namespace std;
 
 int main(int argc , char *argv[])
 {
     WSADATA wsa;
-    SOCKET s , new_socket;
-    struct sockaddr_in server , client;
-    int c;
-    char *message;
-
-
-
-    cout << "\nInitialising Winsock..." << endl;
     if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
     {
-        cout << "Failed. Error Code : " << WSAGetLastError();
-        return 1;
-    }
-    cout << "Initialised" << endl;
-
-    //Create a socket
-    if((s = socket(AF_INET , SOCK_STREAM , 0 )) == INVALID_SOCKET)
-    {
-        cout << "Could not create socket : " << WSAGetLastError();
-    }
-    cout << "Socket created." << endl;
-
-    //Prepare the sockaddr_in structure
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 666 );
-
-    //Bind
-    if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
-    {
-        cout << "Bind failed with error code : " << WSAGetLastError();
-        exit(EXIT_FAILURE);
-    }
-    cout << "Bind done" << endl;
-
-    //Listen to incoming connections
-    listen(s , 3);
-
-    //Accept and incoming connection
-    cout << "Waiting for incoming connections..." << endl;
-
-    c = sizeof(struct sockaddr_in);
-    int recv_size;
-    char server_reply[2000];
-
-    if( (new_socket = accept(s, (struct sockaddr *)&client, &c)) != INVALID_SOCKET )
-    {
-        cout << "Connection accepted" << endl;
-
-        //Reply to the client
-        message = "Hello Client , I have received your connection\n";
-        send(new_socket , message , strlen(message) , 0);
-        while (1){
-        if((recv_size = recv(new_socket, server_reply , 2000 , 0)) == SOCKET_ERROR)
-        {
-        cout << "recv failed" << endl;
-        }
-        server_reply[recv_size] = '\0';
-        cout << "Reply received: " << server_reply << endl;
-        send(new_socket , "message2" , 8 , 0);
-        Sleep(1000);
-        }
-    }
-
-    if (new_socket == INVALID_SOCKET)
-    {
-        cout << "accept failed with error code : " << WSAGetLastError();
+        cout << "Failed to initialise Winsock. Error Code : " << WSAGetLastError();
         return 1;
     }
 
-    closesocket(s);
+    int nPort = 5000;
+    SOCKET serverSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (INVALID_SOCKET == serverSock) {
+        cout << "Invalid socket!" << endl;
+        return 1;
+    }
+
+    SOCKADDR_IN serverSockAddr;
+    serverSockAddr.sin_port = htons(nPort);
+    serverSockAddr.sin_family = AF_INET;
+    serverSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(serverSock, (LPSOCKADDR)&serverSockAddr, sizeof(serverSockAddr)) == SOCKET_ERROR) {
+        cout << "Cannot bind to port " << nPort << endl;
+        closesocket(serverSock);
+        WSACleanup();
+        return 1;
+    }
+
+    u_long iMode = 1;
+    int iResult = ioctlsocket(serverSock, FIONBIO, &iMode);
+    if (iResult != NO_ERROR) {
+        cout << "ioctlsocket failed with error: " << iResult << endl;
+        closesocket(serverSock);
+        WSACleanup();
+        return 1;
+    }
+
+    if (listen(serverSock, SOMAXCONN) == SOCKET_ERROR) {
+        cout << "socket listen error " << WSAGetLastError() << endl;
+        closesocket(serverSock);
+        WSACleanup();
+        return 1;
+    }
+
+    vector <Client> clients();
+
+
+
+
+
+
+
+
     WSACleanup();
 
 
